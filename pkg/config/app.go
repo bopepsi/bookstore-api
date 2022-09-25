90a3f5b796e1a47827516ec930a6f359e5036510
bopepsi <bopepsi@gmail.com>
2022-09-25 15:49:25 -0400

    init-db

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() *gorm.DB {
+	d, err := gorm.Open("postgres", "host=localhost port=5432 user=bopepsi dbname=books password=")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+	fmt.Println("connected db")
+	return d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"github.com/bopepsi/p3-api/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
